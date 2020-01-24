8054d6e0014ad5291b8785312d7177a7d07201a0
William Blanc <blaksmith57@gmail.com>
2020-01-24 16:33:05 -0500

    Fixes some spelling mistakes in broken file

--- PID_balance_arduino/PID_balance_arduino.ino
+++ PID_balance_arduino/PID_balance_arduino.ino
@@ -25,13 +25,9 @@ Servo left_prop;
 
 int16_t Acc_rawX, Acc_rawY, Acc_rawZ,Gyr_rawX, Gyr_rawY, Gyr_rawZ;
  
-
 float Acceleration_angle[2];
 float Gyro_angle[2];
 float Total_angle[2];
-
-
-
 
 float elapsedTime, time, timePrev;
 int i;
@@ -48,7 +44,7 @@ double kd=2.05;//2.05
 ///////////////////////////////////////////////
 
 double throttle=1300; //initial value of throttle to the motors
-float desired_angle = 0; //This is the angle in which we whant the
+float desired_angle = 0; //This is the angle in which we want the
                          //balance to stay steady
 
 
@@ -76,8 +72,8 @@ void setup() {
       ;
   }
   
-  right_prop.attach(3); //attatch the right motor to pin 3
-  left_prop.attach(5);  //attatch the left motor to pin 5
+  right_prop.attach(3); //attach the right motor to pin 3
+  left_prop.attach(5);  //attach the left motor to pin 5
 
   time = millis(); //Start counting time in milliseconds
   /*In order to start up the ESCs we have to send a min value
@@ -113,32 +109,11 @@ void loop() {
     time = millis();  // actual time read
     elapsedTime = (time - timePrev) / 1000; 
   
-  /*The tiemStep is the time that elapsed since the previous loop. 
+  /* The timeStep is the time that elapsed since the previous loop. 
    * This is the value that we will use in the formulas as "elapsedTime" 
-   * in seconds. We work in ms so we haveto divide the value by 1000 
-   to obtain seconds*/
-
-  /*Reed the values that the accelerometre gives.
-   * We know that the slave adress for this IMU is 0x68 in
-   * hexadecimal. For that in the RequestFrom and the 
-   * begin functions we have to put this value.*/
-   
-//     Wire.beginTransmission(0x68);
-//     Wire.write(0x28); //Ask for the 0x3B register- correspond to AcX
-//     Wire.endTransmission(false);
-//     Wire.requestFrom(0x68,6,true); 
-   
-   /*We have asked for the 0x3B register. The IMU will send a brust of register.
-    * The amount of register to read is specify in the requestFrom function.
-    * In this case we request 6 registers. Each value of acceleration is made out of
-    * two 8bits registers, low values and high values. For that we request the 6 of them  
-    * and just make then sum of each pair. For that we shift to the left the high values 
-    * register (<<) and make an or (|) operation to add the low values.*/
-    
-//     Acc_rawX=Wire.read()<<8|Wire.read(); //each value needs two registres
-//     Acc_rawY=Wire.read()<<8|Wire.read();
-//     Acc_rawZ=Wire.read()<<8|Wire.read();
-
+   * in seconds. We work in ms so we have to divide the value by 1000 
+   * to obtain seconds
+   */
  
     /*///This is the part where you need to calculate the angles using Euler equations///*/
     
@@ -159,25 +134,11 @@ void loop() {
      Acceleration_angle[1] = atan(-1*(imu.ax/16384.0)/sqrt(pow((imu.ay/16384.0),2) + pow((imu.az/16384.0),2)))*rad_to_deg;
  
    /*Now we read the Gyro data in the same way as the Acc data. The adress for the
-    * gyro data starts at 0x43. We can see this adresses if we look at the register map
-    * of the MPU6050. In this case we request just 4 values. W don¡t want the gyro for 
+    * In this case we request just 4 values. W don't want the gyro for 
     * the Z axis (YAW).*/
-    
-//   Wire.beginTransmission(0x68);
-//   Wire.write(0x18); //Gyro data first adress
-//   Wire.endTransmission(false);
-//   Wire.requestFrom(0x68,4,true); //Just 4 registers
-//   
-//   Gyr_rawX=Wire.read()<<8|Wire.read(); //Once again we shif and sum
-//   Gyr_rawY=Wire.read()<<8|Wire.read();
  
    /*Now in order to obtain the gyro data in degrees/seconda we have to divide first
    the raw value by 131 because that's the value that the datasheet gives us*/
-
-   /*---X---*/
-//   Gyro_angle[0] = Gyr_rawX/131.0; 
-   /*---Y---*/
-//   Gyro_angle[1] = Gyr_rawY/131.0;
 
    /*Now in order to obtain degrees we have to multiply the degree/seconds
    *value by the elapsedTime.*/
@@ -190,7 +151,7 @@ void loop() {
    Total_angle[1] = 0.98 *(Total_angle[1] + Gyro_angle[1]*elapsedTime) + 0.02*Acceleration_angle[1];
    
    /*Now we have our angles in degree and values from -10º0 to 100º aprox*/
-    //Serial.println(Total_angle[1]);
+   Serial.println(Total_angle[1]);
 
    
   
